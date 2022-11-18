cb1b5e41cd6eb72cdf42fade93fe9c20fc6e92a6
jaimepsayago <jaime.sayago@gmail.com>
2022-11-18 13:00:59 -0500

    commit update 18112022 crud

---
+++ api-memory/src/main/java/com/kruger/demo/ApiMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMemoryApplication.class, args);
+	}
+
+}
