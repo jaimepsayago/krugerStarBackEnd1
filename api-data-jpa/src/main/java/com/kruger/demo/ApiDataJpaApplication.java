55aec0c928b79f509ad8ac74c3c17e4312a63186
jaimepsayago <jaime.sayago@gmail.com>
2022-11-24 20:39:28 -0500

    commit update data jpa with errors!!!

---
+++ api-data-jpa/src/main/java/com/kruger/demo/ApiDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDataJpaApplication.class, args);
+	}
+
+}
